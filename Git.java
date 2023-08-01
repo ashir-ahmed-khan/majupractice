68b6ede4ff30f37968146f7b2d10ad541cb31dbe
ashir-ahmed-khan <ashirahmedkhan>
2023-08-01 18:54:49 +0500

    new file java

---
+++ Git.java
@@ -0,0 +1,9 @@
+public class Git {
+
+    public static void main(String[] args) {
+        int a = 55;
+        int b= 45;
+        int sum = a + b;
+        System.out.println( "total is = " + sum);
+    }
+}
